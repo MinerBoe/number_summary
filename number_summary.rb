d61561c3bc037ed3c565803c315a9e7928a91f8e
Mats Dale <14minerboe16@gmail.com>
2017-10-06 10:48:51 -0600

    Implement median

--- number_summary.rb
+++ number_summary.rb
@@ -7,3 +7,29 @@ def mean(list)
 	mean = total / length
 	return mean
 end
+
+def median(list)
+	list = list.sort()
+	length = list.length
+	middle = 0
+
+	if length % 2 == 0
+
+		upper_value = list[length / 2]
+		lower_value = list[(length / 2) - 1]
+		if upper_value == lower_value
+			return upper_value
+		end
+		avg_value = (upper_value + lower_value) / 2
+		return avg_value
+	end
+
+	if length % 2 != 0 
+		# odd length
+		middle = (length + 1) / 2
+		return list[middle-1]
+	end
+
+
+
+end
