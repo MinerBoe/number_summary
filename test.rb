46497d5cf8bb1a03ac58cde4cd52ce4571a3cf18
Mats Dale <14minerboe16@gmail.com>
2017-10-06 10:48:40 -0600

    Add tests for median

--- test.rb
+++ test.rb
@@ -7,4 +7,19 @@ describe 'number_summary' do
 			mean([15, 10, 25]).must_equal(50/3)
 		end
 	end
+
+	describe 'median' do
+		it 'one value is the median' do
+			median([15, 10, 25]).must_equal(15)
+		end
+		it 'testing one median with more values' do
+			median([12, 13, 14, 15, 16]).must_equal(14)
+		end
+		it 'average of middle two values' do
+			median([10, 14, 16, 20]).must_equal(15)
+		end
+		it 'two values are the same' do
+			median([10, 15, 15, 20]).must_equal(15)
+		end
+	end
 end
